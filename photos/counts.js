5c6dbf4f66a26efee424333018134fb49419efc1
Justin Choi <joonseo.choi@klleon.io>
2026-07-08 11:06:21 +0900

    photos: hawaii +4 new (optimized, gap #34 filled, appended 42-44); counts.js hawaii=44

--- photos/counts.js
+++ photos/counts.js
@@ -1,1 +1,1 @@
-window.PHOTO_COUNTS={"capecod":24,"chicago":28,"japan":49,"hawaii":41,"california":32,"seoul":37};
+window.PHOTO_COUNTS={"capecod":24,"chicago":28,"japan":49,"hawaii":44,"california":32,"seoul":37};
